0acd2eb244aca3fa5c0a60af9d2d21503d2bbc2c
Sajin M Prasad <sajinprasadkm@gmail.com>
2012-05-27 13:28:51 +0530

    Update sumofnums.c

--- sumofnums.c
+++ sumofnums.c
@@ -1,3 +1,10 @@
+/*This is a program to find the sum of numbers divisible by 3 or 5 below 1000. This is a free program. You are free to
+modify or redistribute it.
+
+Author : Sajin M Prasad
+email  : sajin_prasadkm@yahoo.co.in
+*/
+
 #include<stdio.h>
 main()
 {
